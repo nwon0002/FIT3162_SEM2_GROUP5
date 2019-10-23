ecee0f52224c16daa1e4d0aca62f253c46627aad
kche0028 <54053028+kche0028@users.noreply.github.com>
2019-10-23 21:50:11 +0800

    Added detailed documentation

--- main/app.py
+++ main/app.py
@@ -1,3 +1,9 @@
+"""
+Author: Khai Yuan Chee
+Last updated: 23/10/19
+Description: GUI of the copy-move detection software
+"""
+
 from tkinter import *
 from tkinter import filedialog, ttk, messagebox
 from PIL import ImageTk, Image
@@ -7,16 +13,29 @@ from detector import detect_copy_move, readImage
 IMG_WIDTH = 400
 IMG_HEIGHT = 400
 
-# Function to return an image as a PhotoImage object given its path
+
+
 def getImage(path, width, height):
+    """
+    Function to return an image as a PhotoImage object
+    :param path: A string representing the path of the image file
+    :param width: The width of the image to resize to
+    :param height: The height of the image to resize to
+    :return: The image represented as a PhotoImage object
+    """
     img = Image.open(path)
     img = img.resize((width, height), Image.ANTIALIAS)
 
     return ImageTk.PhotoImage(img)
 
-
+# A class representing the GUI of the copy-move detection software
 class GUI(Frame):
     def __init__(self, parent=None):
+        """
+        Initialize the GUI object. Populate the GUI
+        :param parent: The parent of the Frame
+        :return: None
+        """
         self.uploaded_image = None
 
         # Initialize the frame
@@ -68,8 +87,11 @@ class GUI(Frame):
         self.quitButton.grid(row=6, column=0, columnspan=2, sticky="e", pady=5)
 
 
-    # Function to browse through the computer to upload images
     def browseFile(self):
+        """
+        Function to open a browser for users to select an image
+        :return: None
+        """
         # Only accept jpg and png files
         filename = filedialog.askopenfilename(title="Select an image", filetype=[("Image file", "*.jpg *.png")])
 
@@ -86,13 +108,10 @@ class GUI(Frame):
         self.imagePanel.configure(image=img)
         self.imagePanel.image = img
 
-
         # Display blank image in resultPanel
         blank_img = getImage("images/blank.png", IMG_WIDTH, IMG_HEIGHT)
         self.resultPanel.configure(image=blank_img)
         self.resultPanel.image = blank_img
-
-
 
         # Reset the resultLabel
         self.resultLabel.configure(text="READY TO SCAN", foreground="black")
@@ -100,6 +119,10 @@ class GUI(Frame):
 
     # Function to run the program the copy-move detection algorithm
     def runProg(self):
+        """
+        Function to run the copy-move detection algorithm on an uploaded image
+        :return: None
+        """
         # Retrieve the path of the image file
         path = self.uploaded_image
 
@@ -137,9 +160,12 @@ class GUI(Frame):
             self.resultLabel.configure(text="ORIGINAL IMAGE", foreground="green")
 
 
-
 # Main Function
 def main():
+    """
+    Main function which runs the Copy-Move detection application
+    """
+    
     # Initialize the app window
     root = Tk()
     root.title("Copy-Move Detector")
@@ -148,10 +174,13 @@ def main():
     # Ensure the program closes when window is closed
     root.protocol("WM_DELETE_WINDOW", root.quit)
 
+    # Maximize the size of the window
     root.state("zoomed")
 
+    # Add the GUI into the Tkinter window
     GUI(parent=root)
 
+    # Open the GUI
     root.mainloop()
 
 
